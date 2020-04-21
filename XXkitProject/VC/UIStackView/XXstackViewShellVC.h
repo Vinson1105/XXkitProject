10918ed6a00059a64c893c0ec5de8ae9399e87b1
Vinson1105 <386820683@qq.com>
2020-04-22 07:18:58 +0800

    调整文件

---
+++ XXkitProject/VC/UIStackView/XXstackViewShellVC.h
@@ -0,0 +1,17 @@
+//
+//  XXstackViewShellVC.h
+//  XXkitProject
+//
+//  Created by 郭文轩 on 2020/4/22.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XXstackViewShellVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XXkitProject/VC/UIStackView/XXstackViewShellVC.m
@@ -0,0 +1,32 @@
+//
+//  XXstackViewShellVC.m
+//  XXkitProject
+//
+//  Created by 郭文轩 on 2020/4/22.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import "XXstackViewShellVC.h"
+
+@interface XXstackViewShellVC ()
+
+@end
+
+@implementation XXstackViewShellVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
