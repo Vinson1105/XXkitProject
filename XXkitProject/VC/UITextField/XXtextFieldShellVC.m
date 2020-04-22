90af61773f9f3dfc285de10bcfa73aefadecfcc8
Vinson1105 <386820683@qq.com>
2020-04-23 07:04:21 +0800

    新增资源

--- XXkitProject/VC/UITextField/XXtextFieldShellVC.m
+++ XXkitProject/VC/UITextField/XXtextFieldShellVC.m
@@ -8,18 +8,30 @@
 
 #import "XXtextFieldShellVC.h"
 #import "../../../../XXkit/Object-C/UITextField/TextFieldExpressShell.h"
+#import "../../../../XXkit/Object-C/UITextField/TextFieldPasswordShell.h"
+#import "../../../../XXkit/Object-C/UITextField/XXtextFieldShell.h"
 
 @interface XXtextFieldShellVC ()
 @property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITextField *textField2;
 @property (nonatomic,strong) TextFieldExpressShell *expressShell;
+@property (nonatomic,strong) TextFieldPasswordShell *passwordShell;
+@property (nonatomic,strong) XXtextFieldShell *shell2;
 @end
 
 @implementation XXtextFieldShellVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _expressShell = [TextFieldExpressShell new];
-    [_expressShell shell:_textField];
+    _passwordShell = [TextFieldPasswordShell new];
+    [_passwordShell shell:_textField];
+    _passwordShell.clearImage = [UIImage imageNamed:@"common_btn_cancel"];
+    _passwordShell.secureONImage = [UIImage imageNamed:@"common_btn_pwhide"];
+    _passwordShell.secureOFFImage = [UIImage imageNamed:@"common_btn_pwshow"];
+
+
+    _shell2 = [XXtextFieldShell new];
+    [_shell2 shell:_textField2];
     // Do any additional setup after loading the view.
 }
 
