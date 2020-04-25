f614734c11aae9e741b3b318b7becba71b18b821
vinson1105 <vinson1105@foxmail.com>
2020-04-25 17:27:04 +0800

    更新资源，调整文件

--- XXkitProject/VC/UITextField/XXtextFieldShellVC.m
+++ XXkitProject/VC/UITextField/XXtextFieldShellVC.m
@@ -8,12 +8,25 @@
 
 #import "XXtextFieldShellVC.h"
 #import "../../../../XXkit/Object-C/UITextField/XXtextFieldShell.h"
+#import "../../../../XXkit/Object-C/Shell/XXloginShell.h"
+#import "../../../../XXkit/Object-C/XXocUtils.h"
 
+#define kBarViewBgColorON   [XXocUtils colorFromHexString:@"#33cccc"]
+#define kBarViewBgColorOFF  [XXocUtils colorFromHexString:@"#f6f6ee"]
+
+ 
 @interface XXtextFieldShellVC ()
-@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITextField *textField1;
 @property (weak, nonatomic) IBOutlet UITextField *textField2;
 @property (nonatomic,strong) XXtextFieldShell *shell1;
 @property (nonatomic,strong) XXtextFieldShell *shell2;
+
+@property (weak, nonatomic) IBOutlet UITextField *textField3;
+@property (weak, nonatomic) IBOutlet UITextField *textField4;
+@property (weak, nonatomic) IBOutlet UIView *textField3BarView;
+@property (weak, nonatomic) IBOutlet UIView *textField4BarView;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (nonatomic,strong) XXloginShell *mijiaLoginShell;
 @end
 
 @implementation XXtextFieldShellVC
@@ -22,20 +35,44 @@
     [super viewDidLoad];
     
     _shell1 = [XXtextFieldShell new];
-    [_shell1 shell:_textField];
-    [_shell1 configLogo:[UIImage imageNamed:@"username"] size:CGSizeMake(30, 30)];
-    [_shell1 configClear:[UIImage imageNamed:@"common_btn_cancel"] size:CGSizeMake(30, 30) margin:5];
+    [_shell1 shell:_textField1];
+    [_shell1 configLogo:[UIImage imageNamed:@"login.account"] size:CGSizeMake(30, 30)];
+    [_shell1 configClear:[UIImage imageNamed:@"login.clear"] size:CGSizeMake(30, 30) margin:5];
 
     _shell2 = [XXtextFieldShell new];
     [_shell2 shell:_textField2];
-    [_shell2 configLogo:[UIImage imageNamed:@"password"] size:CGSizeMake(30, 30)];
-    [_shell2 configClear:[UIImage imageNamed:@"common_btn_cancel"]
-                secureON:[UIImage imageNamed:@"common_btn_pwhide"]
-               secureOFF:[UIImage imageNamed:@"common_btn_pwshow"]
+    [_shell2 configLogo:[UIImage imageNamed:@"login.password"] size:CGSizeMake(30, 30)];
+    [_shell2 configClear:[UIImage imageNamed:@"login.clear"]
+                secureON:[UIImage imageNamed:@"login.secureON"]
+               secureOFF:[UIImage imageNamed:@"login.secureOFF"]
                     size:CGSizeMake(30, 30)
                   margin:5
                  spacing:5];
-    // Do any additional setup after loading the view.
+    
+    _mijiaLoginShell = [XXloginShell new];
+    [_mijiaLoginShell shellAccount:_textField3 password:_textField4];
+    [_mijiaLoginShell configAccountLogo:[UIImage imageNamed:@"login.account"]
+                           passwordLogo:[UIImage imageNamed:@"login.password"]
+                                   size:CGSizeMake(30, 30)];
+    [_mijiaLoginShell configClearImage:[UIImage imageNamed:@"login.clear"]
+                              secureON:[UIImage imageNamed:@"login.secureON"]
+                             secureOFF:[UIImage imageNamed:@"login.secureOFF"]
+                                  size:CGSizeMake(30, 30)
+                                margin:5
+                               spacing:5];
+    XXOC_WS;
+    [_mijiaLoginShell configOKButton:_loginButton action:^(NSString * _Nonnull account, NSString * _Nonnull password) {
+    }];
+    _mijiaLoginShell.accountShell.onEditBegin = ^(XXtextFieldShell * _Nonnull shell) {
+        XXOC_SS;
+        ss.textField3BarView.backgroundColor = kBarViewBgColorON;
+        ss.textField4BarView.backgroundColor = kBarViewBgColorOFF;
+    };
+    _mijiaLoginShell.passwordShell.onEditBegin = ^(XXtextFieldShell * _Nonnull shell) {
+        XXOC_SS;
+        ss.textField3BarView.backgroundColor = kBarViewBgColorOFF;
+        ss.textField4BarView.backgroundColor = kBarViewBgColorON;
+    };
 }
 
 /*
