2b40632ba4812d96759a93a126b38378ff313432
vinson1105 <vinson1105@foxmail.com>
2020-05-08 17:29:50 +0800

    新增AudioRecordAndPlayViewController

---
+++ XXkitProject/VC/AV/AudioRecordAndPlayViewController.m
@@ -0,0 +1,35 @@
+//
+//  AudioRecordAndPlayViewController.m
+//  XXkitProject
+//
+//  Created by VINSON on 2020/5/8.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import "AudioRecordAndPlayViewController.h"
+#import "../../../../XXkit/Object-C/AV/XXaudioPlayer.h"
+#import "../../../../XXkit/Object-C/AV/XXaudioFormat.h"
+#import "../../../../XXkit/Object-C/AV/xxaudio"
+
+@interface AudioRecordAndPlayViewController ()
+
+@end
+
+@implementation AudioRecordAndPlayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
