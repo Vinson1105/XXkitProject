267f350830ac1ff6ef351c97dc33fe78a5cd4b92
vinson1105 <vinson1105@foxmail.com>
2020-05-09 18:03:50 +0800

    调整文件

--- XXkitProject/VC/AV/AudioRecordAndPlayViewController.m
+++ XXkitProject/VC/AV/AudioRecordAndPlayViewController.m
@@ -9,10 +9,13 @@
 #import "AudioRecordAndPlayViewController.h"
 #import "../../../../XXkit/Object-C/AV/XXaudioPlayer.h"
 #import "../../../../XXkit/Object-C/AV/XXaudioFormat.h"
-#import "../../../../XXkit/Object-C/AV/xxaudio"
+#import "../../../../XXkit/Object-C/AV/XXaudioFileRecorder.h"
+#import "../../../../XXkit/Object-C/XXocUtils.h"
 
 @interface AudioRecordAndPlayViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic,copy) NSString *dir;
 @end
 
 @implementation AudioRecordAndPlayViewController
@@ -20,6 +23,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSError *error;
+    _dir = [XXocUtils mkdirInDocument:@[@"XXkitProject",@"Audio"] error:&error];
+    if(error){
+        NSLog(@"[AudioRecordAndPlayViewController] 创建目录失败");
+    }
+    
+    [_startButton setBackgroundColor:UIColor.greenColor];
+    
+    XXaudioFormat *format = [XXaudioFormat new];
+    format.sampleRate = 16000;
+    format.channels = 2;
+    format.sampleBitSize = 16;
+    format.formatID = kAudioFormatMPEG4AAC;
+    format.formatFlags = kAudioFormatFlagIsSignedInteger;
+    [[XXaudioFileRecorder sharedInstance] config:format];
 }
 
 /*
@@ -31,5 +49,29 @@
     // Pass the selected object to the new view controller.
 }
 */
+- (IBAction)onButtonTouchUpInside:(id)sender {
+    UIButton *button = sender;
+    if(button == _startButton){
+        if([XXaudioFileRecorder sharedInstance].isRunning){
+            [_startButton setBackgroundColor:UIColor.greenColor];
+            NSURL *url = [[XXaudioFileRecorder sharedInstance] stop];
+            NSLog(@"[AudioRecordAndPlayViewController] 录音停止，文件保存到：%@",url);
+        }
+        else{
+            NSString *name = [[XXocUtils currentDateString] stringByAppendingString:@".aac"];
+            NSString *path = [_dir stringByAppendingPathComponent:name];
+            NSURL *url = [NSURL fileURLWithPath:path];
+            if([[XXaudioFileRecorder sharedInstance] start:url]){
+                NSLog(@"[AudioRecordAndPlayViewController] 开始成功");
+                [_startButton setBackgroundColor:UIColor.redColor];
+            }
+            else{
+                NSLog(@"[AudioRecordAndPlayViewController] 开始失败");
+            }
+        }
+    }
+    else{
+    }
+}
 
 @end
