c1059336284716e9687fc062ae0dc2ca49436df8
vinson1105 <vinson1105@foxmail.com>
2020-06-03 11:48:58 +0800

    调整工程

--- XXkitProject/VC/Category/LangaugeViewController.m
+++ XXkitProject/VC/Category/LangaugeViewController.m
@@ -9,11 +9,12 @@
 #import "LangaugeViewController.h"
 #import "../../../../XXkit/Object-C/Category/NSBundle+Language.h"
 #import "../../../../XXkit/Object-C/Shell/XXtableViewShell.h"
+#import "../../../../XXkit/Object-C/XXocUtils.h"
 
 @interface LangaugeViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic,strong) XXtableViewShell *tableShell;
+@property (nonatomic,copy) NSIndexPath *indexPath;
 @end
 
 @implementation LangaugeViewController
@@ -21,24 +22,63 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    _tipLabel.text = NSLocalizedString(@"language.tip", nil);
+    NSString *currentLanguage   = [NSBundle xx_currentLanguage];
+    NSArray *available          = [NSBundle xx_avaliableLanguages];
     
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"en" ofType:@"lproj"];
-    NSLog(@"[Language] %@",path);
-    NSArray *available = [NSBundle xx_avaliableLanguages];
-    NSLog(@"[Language] %@",available);
-    NSLog(@"[Language] %@",[NSBundle mainBundle].resourcePath);
-
+    _tableShell = [XXtableViewShell new];
+    [_tableShell shell:_tableView];
+    [_tableShell configRowType:nil loadType:0 systemStyle:UITableViewCellStyleDefault height:57];
+    
+    NSMutableArray *row = [NSMutableArray new];
+    NSMutableDictionary *rowInfo = [[NSMutableDictionary alloc] initWithDictionary:@{
+        @"Title":NSLocalizedString(@"language.system", nil),
+        @"Language":@"System",
+        @"AccessoryType":nil==currentLanguage?@(UITableViewCellAccessoryCheckmark):@(UITableViewCellAccessoryNone)
+    }];
+    [row addObject:rowInfo];
+    for (NSString *language in available) {
+        BOOL isUsing = nil != currentLanguage && [currentLanguage isEqualToString:language];
+        rowInfo = [[NSMutableDictionary alloc] initWithDictionary:@{
+            @"Title":NSLocalizedString(([NSString stringWithFormat:@"language.%@",language]), nil),
+            @"Language":language,
+            @"AccessoryType":isUsing?@(UITableViewCellAccessoryCheckmark):@(UITableViewCellAccessoryNone)
+        }];
+        [row addObject:rowInfo];
+        
+        if(isUsing){
+            _indexPath = [NSIndexPath indexPathForRow:row.count-1 inSection:0];
+        }
+    }
+    if(nil == currentLanguage){
+        _indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    }
+    
+    [_tableShell configSectionWithHeaders:nil rows:@[row] footers:nil];
+    XXOC_WS;
+    _tableShell.onRowClicked = ^(XXtableViewShell * _Nonnull shell, NSIndexPath * _Nonnull indexPath, id  _Nonnull data) {
+        XXOC_SS;
+        
+        /// 清除之前的状态
+        NSIndexPath *lastIndexPath = ss.indexPath;
+        NSMutableDictionary *info = [shell getDataAtIndexPath:lastIndexPath];
+        if(nil != info){
+            info[@"AccessoryType"] = @(UITableViewCellAccessoryNone);
+        }
+        
+        /// 设置当前选中状态
+        info = data;
+        if(nil != info){
+            info[@"AccessoryType"] = @(UITableViewCellAccessoryCheckmark);
+            ss.indexPath = indexPath;
+        }
+        
+        /// 更新状态
+        [shell.tableView reloadRowsAtIndexPaths:@[indexPath,lastIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        
+        /// 设置
+        NSString *language = info[@"Language"];
+        BOOL isSystem = [language isEqualToString:@"System"];
+        [NSBundle xx_setLanguage:isSystem?nil:language];
+    };
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
