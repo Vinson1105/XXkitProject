864d3917909cc492f91e9ccf6d6a6500b558ba67
vinson1105 <vinson1105@foxmail.com>
2020-07-13 20:00:49 +0800

    更新工程

--- XXkitProject/VC/AV/VideoRecordViewController.m
+++ XXkitProject/VC/AV/VideoRecordViewController.m
@@ -14,14 +14,19 @@
 @interface VideoRecordViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIButton *button;
+@property (nonatomic,strong) UIView *preview;
 @end
 
 @implementation VideoRecordViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.preview = [[UIView alloc] initWithFrame:CGRectMake(0, 50, 50, 100)];
+    self.preview.backgroundColor = UIColor.blackColor;
+    [self.view insertSubview:self.preview aboveSubview:self.tableView];
+    
     // Do any additional setup after loading the view.
-    [XXvideoRecorder sharedInstance].preview = self.view;
+    [XXvideoRecorder sharedInstance].preview = self.preview;
     [_button setBackgroundColor:UIColor.greenColor forState:UIControlStateNormal];
     [_button setBackgroundColor:UIColor.redColor forState:UIControlStateSelected];
 }
@@ -43,7 +48,7 @@
         button.selected = NO;
     }
     else{
-        [XXocUtils anthorizedCameraCheckAtViewController:self message:@"请求摄像机权限" succeed:^{
+        [XXocUtils anthorizedCameraCheckAtViewController:self title:@"请求权限" message:@"请求摄像机权限" succeed:^{
             [[XXvideoRecorder sharedInstance] startConnect];
             button.selected = YES;
         }];
