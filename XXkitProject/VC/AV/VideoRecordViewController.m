c9493b4d8aae1375a797934f62b741572e0693c7
vinson1105 <vinson1105@foxmail.com>
2020-07-14 17:56:52 +0800

    Update VideoRecordViewController.m

--- XXkitProject/VC/AV/VideoRecordViewController.m
+++ XXkitProject/VC/AV/VideoRecordViewController.m
@@ -9,26 +9,35 @@
 #import "VideoRecordViewController.h"
 #import "../../../../XXkit/Object-C/AV/XXvideoRecorder.h"
 #import "../../../../XXkit/Object-C/Category/UIButton+StateSettings.h"
+#import "../../../../XXkit/Object-C/Shell/XXviewMovableShell.h"
 #import "XXocUtils.h"
 
 @interface VideoRecordViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIButton *button;
 @property (nonatomic,strong) UIView *preview;
+@property (nonatomic,strong) XXviewMovableShell *movableShell;
 @end
 
 @implementation VideoRecordViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.preview = [[UIView alloc] initWithFrame:CGRectMake(0, 50, 50, 100)];
+    
+    self.preview = [[UIView alloc] initWithFrame:CGRectMake(50, 50, 50, 100)];
     self.preview.backgroundColor = UIColor.blackColor;
     [self.view insertSubview:self.preview aboveSubview:self.tableView];
     
-    // Do any additional setup after loading the view.
     [XXvideoRecorder sharedInstance].preview = self.preview;
     [_button setBackgroundColor:UIColor.greenColor forState:UIControlStateNormal];
     [_button setBackgroundColor:UIColor.redColor forState:UIControlStateSelected];
+    
+    self.movableShell = [XXviewMovableShell new];
+    [self.movableShell shell:self.preview];
+}
+- (void)dealloc{
+    [XXvideoRecorder sharedInstance].preview = nil;
+    [[XXvideoRecorder sharedInstance] stopConnect];
 }
 
 /*
