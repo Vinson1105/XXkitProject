ae12d8ec3f9c86f9734c40cb0861deefb4093c22
Vinson1105 <386820683@qq.com>
2020-10-19 07:25:18 +0800

    update

---
+++ XXkitProject/APP/AccountLogin/AccountLoginVC.m
@@ -0,0 +1,32 @@
+//
+//  AccountLoginVC.m
+//  XXkitProject
+//
+//  Created by 郭文轩 on 2020/10/17.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import "AccountLoginVC.h"
+
+@interface AccountLoginVC ()
+
+@end
+
+@implementation AccountLoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
