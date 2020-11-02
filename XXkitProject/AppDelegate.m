597af29719e4a0197d214c0c08d82fdff6d74bab
vinson1105 <vinson1105@foxmail.com>
2020-11-02 18:00:55 +0800

    update

--- XXkitProject/AppDelegate.m
+++ XXkitProject/AppDelegate.m
@@ -8,10 +8,27 @@
 
 #import "AppDelegate.h"
 #import "./MainViewController.h"
+#import "../../XXkit/Object-C/Quick/QuickLoader.h"
+
+//#define Main
+#define XXAPP
+
+#ifdef Main
+#define kMainStoryboard @"Main"
+
+#elif defined XXAPP
+#define kMainStoryboard @"XXAPP"
+#endif
+
+//#define kMainStoryboard @"Main"
 
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey,id> *)launchOptions{
-    [self createNavigationWithIntitalViewControllerAtStoryboard:@"Main" bundle:nil];
+#ifdef XXAPP
+    [QuickLoader setMainQuickDataWithFileName:@"XXAPP.json" bundle:nil];
+#endif
+    [self createNavigationWithIntitalViewControllerAtStoryboard:kMainStoryboard bundle:nil];
+
     return YES;
 }
 @end
